cfaef59eca5955a7ed564baa4ae106c5b563a08b
Matt McCormick <matt.mccormick@kitware.com>
2014-08-20 23:55:33 -0400

    Add run.sh.

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if test $# -lt 1; then
+	# Get the latest opengl-nvidia build
+	# and start with an interactive terminal enabled
+	args="-i -t $(docker images | grep ^opengl-nvidia | head -n 1 | awk '{ print $1":"$2 }')"
+else
+	args="$@"
+fi
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+docker run \
+	--privileged \
+	-v $XSOCK:$XSOCK:rw \
+	-v $XAUTH:$XAUTH:rw \
+	-v /dev/dri:/dev/dri:rw \
+	-e DISPLAY=$DISPLAY \
+	-e XAUTHORITY=$XAUTH \
+	$args
