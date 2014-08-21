aa4b206e8de0268d5363b2b3721eb59bafe0c20d
Matt McCormick <matt.mccormick@kitware.com>
2014-08-21 00:23:12 -0400

    Suggest using run.sh with derived images.

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ if test $# -lt 1; then
 	# and start with an interactive terminal enabled
 	args="-i -t $(docker images | grep ^opengl-nvidia | head -n 1 | awk '{ print $1":"$2 }')"
 else
+        # Use this script with derived images, and pass your 'docker run' args
 	args="$@"
 fi
 
