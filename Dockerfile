eec9ec1129772cd1db5741a162370d39a2a0eb75
Matt McCormick <matt.mccormick@kitware.com>
2014-08-20 23:18:36 -0400

    Add Dockerfile and build.sh.

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM debian
+MAINTAINER Matt McCormick <matt@mmmccormick.com>
+
+RUN apt-get update
+RUN apt-get -y --force-yes install x-window-system
+RUN apt-get install -y binutils
+RUN apt-get install -y mesa-utils
+RUN apt-get install -y module-init-tools
+
+ADD nvidia-driver.run /tmp/nvidia-driver.run
+RUN sh /tmp/nvidia-driver.run -a -N --ui=none --no-kernel-module
+RUN rm /tmp/nvidia-driver.run
+
+CMD /bin/bash
---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# We must use the same driver in the image as on the host
+if test ! -f nvidia-driver.run; then
+  # Get your current host nvidia driver version, e.g. 340.24
+  nvidia_version=$(cat /proc/driver/nvidia/version | head -n 1 | awk '{ print $8 }')
+  nvidia_driver_uri=http://us.download.nvidia.com/XFree86/Linux-x86_64/${nvidia_version}/NVIDIA-Linux-x86_64-${nvidia_version}.run
+  wget -O nvidia-driver.run $nvidia_driver_uri
+fi
+
+docker build -t opengl-nvidia-${nvidia-version} .
